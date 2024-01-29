879142b0a6a25e987b5bab55a2af211f864300da
nomusic44989@yandex.ru <nomusic44989@yandex.ru>
2024-01-29 23:23:08 +0300

    сохранил

--- main.js
+++ main.js
@@ -1,9 +1,52 @@
-const marquee = document.querySelector('.marquee');
-const block = marquee.querySelector('.marquee__container');
+(function () {
+	'use strict';
+	let $;
+	function Slider (container, params) {	
+		let s = document.querySelector(container);
+		let list = s.querySelector(".lists");
 
-marquee.addEventListener('mouseenter', () => {
-	block.style.animationPlayState = 'paused';
-});
-marquee.addEventListener('mouseleave', () => {
-	block.style.animationPlayState = 'running';
-});
+		if(params.pagination && typeof params.pagination === 'string'){
+			let paginationElement = document.querySelector(params.pagination);
+			let buttonPrev = paginationElement.querySelector("#prev");
+			let buttonNext = paginationElement.querySelector("#next");
+		}
+
+		if(params.dots && typeof params.dots === 'string'){
+			let dots = document.querySelector(params.dots);
+		}
+
+		// Делаем копию 
+		let listItems = list.children;
+		const addClonesToEnd = (clonesNumber, itemsList) => {
+			for (let i = 0; i < clonesNumber; i++) {
+				let cloneItem = itemsList[i].cloneNode(true);
+				cloneItem.classList.remove("active");
+				itemsList[i].parentNode.appendChild(cloneItem);
+			}
+		};
+		addClonesToEnd(3, listItems);
+
+		/* DEFAULT INDEXES */
+		let currentSlideIndex = 0;
+		let nextSlideIndex = 1;
+
+		let listItem = listItems[currentSlideIndex];
+		let nextPicItem = listItems[nextSlideIndex];
+
+		/* Function Click */
+		const getItemsByIndex = () => {
+			slidItem = listItems[currentSlideIndex];	
+			nextslidItem = listItems[nextSlideIndex];
+		};
+
+		const switchClasses = () => {
+			slidItem.classList.remove("active");
+			nextslidItem.classList.add("active");
+		};
+
+		console.log(nextPicItem);
+		//return s;
+	}
+
+	window.Slider = Slider;
+})();
